9ace2b100bac062512cc4c5fb57596ffc2f5a30a
Jaime <julianarueda40@gmail.com>
2024-06-03 00:29:50 -0300

    otro fix header

--- components/header.js
+++ components/header.js
@@ -15,28 +15,21 @@ class Header extends HTMLElement {
                 <span class="navbar-toggler-icon"></span>
             </button>
             <!-- Contenido del Navbar -->
+
             <div class="collapse navbar-collapse" id="navbarNav">
-                <!-- Sección Centro -->
-                <ul class="navbar-nav nav-center">
-                    <li class="nav-item">
-                    <span><i class="bi bi-house-door-fill"></i></span>
-                        <a class="nav-link" href="`+BASE_PATH+`"><i class="fa fa-home" aria-hidden="true"></i>&nbsp; Inicio</a>
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="`+BASE_PATH+`templates/historia.html"><i class="fa fa-book" aria-hidden="true"></i>&nbsp; Historia</a>
+                        <a class="nav-link" href="#">Link</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="`+BASE_PATH+`templates/premios.html"><i class="fa fa-trophy" aria-hidden="true"></i>&nbsp; Premios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="`+BASE_PATH+`templates/favorito.html"><i class="fa fa-heart" aria-hidden="true"></i>&nbsp; Tu Favorito</a>
+                        <a class="nav-link disabled" href="#">Disabled</a>
                     </li>
                 </ul>
-                <!-- Sección Derecha -->
-                <span class="navbar-text ml-auto">
-                    <img src="`+BASE_PATH+`static/img/icono-alfajor.png" alt="Icono de Alfajor" style="width: 40px; height: auto;">
-                </span>
             </div>
+            
         </div>
         </nav>
     </header>
